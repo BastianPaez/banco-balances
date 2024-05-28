0deb6aa83b3f30ba620a9ea132f5c8892cf6b1f8
Bastian <bpt.bastian@gmail.com>
2024-05-28 00:46:39 -0400

    corregido

---
+++ connection/tablas.sql
@@ -0,0 +1,14 @@
+CREATE TABLE usuarios (
+	id SERIAL PRIMARY KEY,
+	nombre VARCHAR(50),
+	balance FLOAT CHECK (balance >= 0));
+
+CREATE TABLE transferencias (
+	id SERIAL PRIMARY KEY,
+	emisor INT,
+	receptor INT,
+	monto FLOAT,
+	fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (emisor) REFERENCES usuarios(id),
+	FOREIGN KEY (receptor) REFERENCES usuarios(id));
+	
